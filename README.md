88fab5e0770cbfdf19f2a277282af72f30fa96ec
James Kelly <jamesdkelly88@outlook.com>
2024-11-20 20:00:51 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# NixOS Configuration
+
+## Initial setup
+
+This assumes you are running NixOS with an internet connection.
+
+```sh
+cd ~
+nix-shell -p gh
+gh auth login
+gh repo clone nixos-config
+# edit the hostname if not using LT16
+
+sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
+sudo ln -s /home/james/nixos-config/configuration.nix /etc/nixos/configuration.nix
+sudo nixos-rebuild switch
+```
+
+## System install (to USB / HDD)
+
+Boot the minimal install
+
+```sh
+sudo -s
+blkid # make a note of target device
+ip a # make a note of wifi adapter
+
+wpa_passphrase "SSID" > wifi.conf
+wpa_supplicant -i <wifi adapter e.g. wlp1s0> -c wifi.conf -B
+dhcpcd
+
+fdisk /dev/sdX # where sdX is the target disk
+# g, n, 1, 2048, +M, t, 1, n, 2, <enter>, <enter>, w # GPT with 500MB EFI partition and linux partition filling the rest of the disk
+mkfs.fat -F 32 /dev/sdX1
+fatlabel /dev/sdX1 NIXBOOT
+mkfs.ext4 /dev/sdX2 -L NIXROOT
+mount /dev/disk/by-label/NIXROOT /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/NIXBOOT /mnt/boot
+
+nixos-generate-config --root /mnt
+curl https://raw.githubusercontent.com/jamesdkelly88/nixos-config/refs/heads/main/configuration.nix > /mnt/etc/nixos/configuration.nix
+cd /mnt
+nixos-install
+
+# set root password when prompted
+# reboot
+# login as root
+# set passwd for james
+# switch to user james and lock root user:
+passwd -l root
+```
+
+## TODO
+
+- firefox system certificates
+- samba with SMB1
+- homemanager
+- essential packages
+- system settings
+- try out different desktops (deepin)
---
+++ configuration.nix
@@ -0,0 +1,135 @@
+{ config, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.05.tar.gz";
+  hostname = "lt16";
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      (import "${home-manager}/nixos")
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Define your hostname
+  networking.hostName = "${hostname}"; 
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/London";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Pantheon Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "uk";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.james = {
+    isNormalUser = true;
+    description = "James Kelly";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+  home-manager.users.james = { 
+    home.stateVersion = "24.05";
+    
+    programs.git = {
+        enable = true;
+        userName = "James Kelly";
+        userEmail = "jamesdkelly88@outlook.com";
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
+  # Install firefox. - this certificate stuff isn't working
+  programs.firefox = {
+    enable = true;
+    policies.SecurityDevices.p11-kit-proxy = "${pkgs.p11-kit}/lib/p11-kit-proxy.so";
+  };
+
+  lib.nss = {
+    useP11kit = true;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+    nss
+    p11-kit
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+      ];
+    })
+  ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+}
