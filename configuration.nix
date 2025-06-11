b0f127f4c2bc1b7425ca63ee9f519f46b3373ea1
James Kelly <jamesdkelly88@outlook.com>
2025-06-11 17:33:49 +0100

    upgrade to 25.05

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-24.11.tar.gz";
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
   hostname = "lt16";
   patchelfFixes = pkgs.patchelfUnstable.overrideAttrs (_finalAttrs: _previousAttrs: {
     src = pkgs.fetchFromGitHub {
